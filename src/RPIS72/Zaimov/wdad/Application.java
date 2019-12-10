bac5ddb14ff460b72102014c53d7df887d912fdb
Alexandr1998 <sa_shok_98@mail.ru>
2019-12-10 23:23:49 +0400

    scv

---
+++ src/RPIS72/Zaimov/wdad/Application.java
@@ -0,0 +1,7 @@
+package RPIS72.Zaimov.wdad;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("«I’m Александр Заимов, and I’m not a monkey");
+    }
+}
